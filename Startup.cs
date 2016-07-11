565681f6f875b3d6fe99f4fdf12bab48ba85f1e8
Andrew Vasilyev <a.vasilyev@corvusalba.ru>
2016-07-11 13:54:37 +0000

    fix html

--- Startup.cs
+++ Startup.cs
@@ -15,28 +15,35 @@ namespace PSNBot
                 {
                     var path = context.Request.Path;
 
+                    var html = "\"Ламберт, Ламберт - Хер моржовый, Ламберт, Ламберт - вредный хуй!\" (с) Геральт из Ривии, ведьмак.";
+
                     if (path.StartsWithSegments(new PathString("/trophy"), StringComparison.OrdinalIgnoreCase))
                     {
                         var title = context.Request.Query["title"];
                         var content = context.Request.Query["content"];
                         var image = context.Request.Query["image"];
 
-                        var html = string.Format(@" <!DOCTYPE html>
-                                                    <html xmlns:cc='http://creativecommons.org/ns#'>
-                                                        <head prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#'>
-                                                            <meta content='text/html; charset=utf-8' http-equiv='Content-Type'>
-                                                            <meta content='{0}' name='title'>
-                                                            <meta content='{1}' name='description'>
-                                                            <meta content='{2}' property='og:image'>
+                        html = string.Format(@" <!DOCTYPE html>
+                                                    <html 	xmlns=""http://www.w3.org/1999/xhtml""
+								xmlns:cc=""http://creativecommons.org/ns#""
+								xmlns:fb=""http://ogp.me/ns/fb#"">
+                                                        <head prefix=""og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#"">
+                                                            <meta content=""text/html; charset=utf-8"" http-equiv=""Content-Type"">
+                                                            <meta name=""title"" property=""og:title"" content=""{0}"" />
+                                                            <meta name=""description"" property=""og:description"" content=""{1}"" />
+                                                            <meta property=""og:image"" content=""{2}"" />
                                                         </head>
                                                     <body>
                                                         Никак вы, блядь, не научитесь.
                                                     </body>
                                                 </html>", title, content, image);
-                        return context.Response.WriteAsync(html);
                     }
 
-                    return context.Response.WriteAsync("\"Ламберт, Ламберт - Хер моржовый, Ламберт, Ламберт - вредный хуй!\" (с) Геральт из Ривии, ведьмак.");
+                    context.Response.Headers.Add("Content-Length", System.Text.Encoding.UTF8.GetBytes(html).Length.ToString());
+                    context.Response.Headers.Add("Cache-Control", "max-age=0, no-store");
+                    context.Response.Headers.Add("Content-Type", "text/html; charset=UTF-8");
+
+                    return context.Response.WriteAsync(html);
                 }
                 catch (Exception e)
                 {
